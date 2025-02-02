c206920293c64e79523bb8072234176d76689436
Jazz Turner-Baggs <473256+jazzz@users.noreply.github.com>
2025-02-01 17:10:02 -0800

    Normalize paths in cpp:run_tests.sh

--- tests/cpp/run_tests.sh
+++ tests/cpp/run_tests.sh
@@ -1,8 +1,12 @@
 ###  This script builds and then runs the cpp tests
 ###  Invoke 'run_script.sh <PATH_TO_RAMBLE.HPP>'
 
+# Get script directory to get the correct directory to pass to cmake, regardless of where the script is executed from
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
 if [[ ! -z "$1" ]]; then
-    GENERATED_DIRECTORY=$1
+    # Expand passed in directory incase it was a relative path
+    export GENERATED_DIRECTORY="$(realpath $1)"
 fi 
 
 if [[ -z "${GENERATED_DIRECTORY}" ]]; then
@@ -12,18 +16,15 @@ fi
 
 echo "Using GENERATED_DIRECTORY=$GENERATED_DIRECTORY"
 
-cd ./tests/cpp/
-
-
-cmake .
+cmake $SCRIPT_DIR
 
 retVal=$?
 if [ $retVal -ne 0 ]; then
     echo "Failed to configure cmake" 
     exit $retVal
 fi
-
-cmake --build .
+export CMAKE_VERBOSE_MAKEFILE=True
+cmake --build  $SCRIPT_DIR
 
 retVal=$?
 if [ $retVal -ne 0 ]; then
@@ -31,7 +32,7 @@ if [ $retVal -ne 0 ]; then
     exit $retVal
 fi
 
-./bin/tests
+$SCRIPT_DIR/bin/tests
 
 retVal=$?
 if [ $retVal -ne 0 ]; then
